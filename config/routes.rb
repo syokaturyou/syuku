81228d6f8fa8904ee5458143aa7b524c21e44e7b
syokaturyou <teamsumagaku2@gmail.com>
2022-02-09 10:44:06 +0000

    新規登録/ログイン関連追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'homes#top'
+  
+  
+ resources :users, only: [:index, :edit, :update]
+ get 'users/account' => 'users#account', as: 'account'
+ get 'users/profile' => 'users#profile', as: 'profile'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   
 end
